918903d8e79c833229f2730c4c4061bea63b0564
alanajacdev <adamkrupadev@outlook.com>
2020-12-11 20:17:27 -0800

    Added Hello World Project

---
+++ Hello_Goodbye_World/.lkm_example.mod.cmd
@@ -0,0 +1,1 @@
+cmd_/home/ubuntu2004/src/lkm_example/lkm_example.mod := { echo  /home/ubuntu2004/src/lkm_example/lkm_example.o; echo; } > /home/ubuntu2004/src/lkm_example/lkm_example.mod
---
+++ Hello_Goodbye_World/lkm_example.mod
@@ -0,0 +1,2 @@
+/home/ubuntu2004/src/lkm_example/lkm_example.o
+
